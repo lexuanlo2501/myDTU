7073d892d100bccf815ca869d10ef20f8f02fbe9
Nguyen Thanh Hung <hung@HungDev.Machine>
2022-11-28 23:15:00 +0700

    Xóa cái get schedule vì để không cũng chẳng làm gì và refactor lại code ở module lên lịch cho cron job

--- controllers/system/ system.controller.js
+++ controllers/system/ system.controller.js
@@ -1,62 +1,61 @@
-const cron = require('node-cron');
-const Class = require('../../models/class&courses/classes.mongo');
-const notifications = require('../../models/users/Notifications/notifications');
-const UserNotitfication = require('../../models/users/Notifications/user.notifications');
+const 
+    cron = require('node-cron'),
+    Class = require('../../models/class&courses/classes.mongo'),
+    notifications = require('../../models/users/Notifications/notifications'),
+    UserNotification = require('../../models/users/Notifications/user.notifications');
+
 const scheduleClass = async (req,res)=>{
-    const {semester,year,open_time,close_time,executionType} = req.body;
-    try{
-    // minutes , hours , date , month , week day ,
-    // Chi nen dung de mo lop / dong lop 
+
+const {semester,year,open_time,close_time,executionType} = req.body;
+try{
+    
+    
     if(!cron.validate(open_time) || !cron.validate(close_time)) throw `Định dạng cho cronjob không chính xác`
     
-    const user_noti = await UserNotitfication.findOne({_id:req.user._id});
-    const {Notifications} = user_noti._doc;
     let open_job = cron.schedule(`${open_time}`,async()=>{
-        const res = await Class.updateMany({year,semester},{available:true});
-        const notification = new notifications({
+        const 
+            result = await Class.updateMany({year,semester},{available:true}),
+            notification = new notifications({
             sender:req.user._id,
             receiver:req.user._id,
             Type:'Cập nhập trạng thái mở các lớp',
-            content:`Tại thời điểm : ${Date()} , hệ thống bắt đầu mở đăng ký cho ${res.modifiedCount} lớp học trong ${semester} , ${year}`
+            content:`Tại thời điểm : ${new Date().toLocaleString("en-AU")} , hệ thống bắt đầu mở đăng ký cho ${result.modifiedCount} lớp học trong ${semester} , ${year}`
         });
         await notification.save();
+        await UserNotification.findOneAndUpdate({_id:req.user._id},{$push:{Notifications:notification._id}});
         console.log(`Added new open notification`);
-        Notifications.push(notification._id);
-        await UserNotitfication.findOneAndUpdate({_id:req.user._id},{Notifications:Notifications});
         
 
         if(executionType === 1) {
-            console.log(`Class open job ended at : ${Date()}`);
+            console.log(`Class open job ended at : ${new Date().toLocaleString("en-AU")}`);
             open_job.stop();
             open_job = null;
         }
         
     });
     let close_job = cron.schedule(`${close_time}`,async()=>{
-        console.log(`Class close job started at : ${Date()}`);
-        const res = await Class.updateMany({year,semester},{available:false});
-        console.log(`Updated : ${res.modifiedCount}`);
+        console.log(`Class close job started at : ${new Date().toLocaleDateString("en_AU")}`);
+        const result = await Class.updateMany({year,semester},{available:false});
         const notification = new notifications({
             sender:req.user._id,
             receiver:req.user._id,
             Type:'Cập nhập trạng thái mở các lớp',
-            content:`Tại thời điểm : ${Date()} , hệ thống bắt đầu đóng đăng ký cho ${res.modifiedCount} lớp học trong ${semester} , ${year}`
+            content:`Tại thời điểm : ${Date()} , hệ thống bắt đầu đóng đăng ký cho ${result.modifiedCount} lớp học trong ${semester} , ${year}`
         });
         await notification.save();
+        await UserNotification.findOneAndUpdate({_id:req.user._id},{$push:{Notifications:notification._id}});
+        console.log(`Updated : ${result.modifiedCount}`);
         console.log(`Added new  close notification`);
-        Notifications.push(notification._id);
-        await UserNotitfication.findOneAndUpdate({_id:req.user._id},{Notifications:Notifications});
        
         if(executionType === 1) {
-            console.log(`Class close job ended at : ${Date()}`);
             close_job.stop();
             close_job = null;
+            console.log(`Class close job ended at : ${new Date().toLocaleString("en-AU")}`);
         }
     });
     open_job.start();
     close_job.start();
-  
-      
+    
     res.status(200).json({message:`Bạn đã cài đặt thành công cronjob cập nhập thông tin các lớp tự động`});
 }
     catch (error){
--- routes/admin/admin.router.js
+++ routes/admin/admin.router.js
@@ -3,7 +3,6 @@ const user_router = require('./admin-user/admin-user.router');
 const class_router = require('./study-info/class.router');
 const course_router = require('./study-info/course.router');
 const group_router = require('./study-info/courseGroup.router');
-const schedule_router = require('./study-info/schedule.router');
 const systemSchedule_router = require('./study-info/system.schedule.router');
 Root_router.get('/',(req,res)=>{
     console.log('Admin');
@@ -14,7 +13,6 @@ Root_router.use('/',user_router);
 Root_router.use('/',class_router);
 Root_router.use('/',course_router);
 Root_router.use('/',group_router);
-Root_router.use('/',schedule_router);
 Root_router.use('/',systemSchedule_router)
 
 module.exports = Root_router;
